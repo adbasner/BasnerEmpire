827a6bc5c59ee87047be8fe4755d3b2fbbe1aaa8
Andrew Basner <andrewbasner@gmail.com>
2018-12-15 03:59:35 -0600

    remove unirest and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #pages
+
+  # api routes
+  namespace :admin do
+    namespace :v1 do
+      resources :posts, except: [:new, :edit]
+    end
+  end
+
+  # user routes
+  get '/dashboard/' => 'users#show'
+  resources :users, only: [:edit, :update]
+
+  # sessions routes
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
